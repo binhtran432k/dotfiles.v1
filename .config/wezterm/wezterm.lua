5297e026396f04679957a77ee04158c95aa6784c
Tran Duc Binh <binhtran432k@gmail.com>
2022-08-15 22:25:50 +0700

    update wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,5 +1,9 @@
 local wezterm = require("wezterm")
 
 return {
-    color_scheme = "dracula",
+   color_scheme = "Dracula",
+   font_size = 12,
+   window_background_opacity = 0.9,
+   hide_tab_bar_if_only_one_tab = true,
+   enable_wayland = false,
 }
