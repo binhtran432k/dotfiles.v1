089dcbeda4d5fe93d455c046e0fdd6c4219f068a
Tran Duc Binh <binhtran432k@gmail.com>
2023-07-14 16:53:47 +0700

    zsh: update alias list

--- config/zsh/alias.sh
+++ config/zsh/alias.sh
@@ -2,7 +2,7 @@ alias v=nvim
 alias nvimm="nvim -u /etc/vimrc"
 alias lg=lazygit
 
-# Make ranger source directory
 alias sclear="printf '\033[2J\033[3J\033[1;1H'"
 alias paru-devel="pacman -Qmq | grep -Ee '-(cvs|svn|git|hg|bzr|darcs|nightly-bin)$' | paru -Ta - | paru -S --needed -"
-alias upnvim="pamac install neovim-nightly-bin"
+
+alias ts="npx tree-sitter"
